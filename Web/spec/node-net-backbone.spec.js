342ed893221618f69b311f08dc262a0136824154
Daniel Dosen <ddosen@gmail.com>
2012-02-14 22:32:15 -0800

    working with foreman

--- Web/spec/node-net-backbone.spec.js
+++ Web/spec/node-net-backbone.spec.js
@@ -1,26 +1,8 @@
 (function() {
 
   describe("Sample Test", function() {
-    it("should pass becuase it does nothing", function() {
+    return it("should pass becuase it does nothing", function() {
       return expect("hello").toEqual("hello");
-    });
-    it("should pass  becuase it does nothing", function() {
-      return expect("goodbye").not.toEqual("hello");
-    });
-    it("shows asynchronous test", function() {
-      setTimeout((function() {
-        expect("second").toEqual("second");
-        return asyncSpecDone();
-      }), 1);
-      expect("first").toEqual("first");
-      return asyncSpecWait();
-    });
-    return it("shows asynchronous test node-style", function(done) {
-      setTimeout((function() {
-        expect("second").toEqual("second");
-        return done();
-      }), 1);
-      return expect("first").toEqual("first");
     });
   });
 
