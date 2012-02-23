9b4a4a5e2ad1aa701ecd88468eb92769667b44d8
dgdosen <ddosen@gmail.com>
2012-02-23 08:25:38 -0800

    adding data classes to web service

--- WebService/RegistryPatient.cs
+++ WebService/RegistryPatient.cs
@@ -5,6 +5,9 @@ using System.ServiceModel;
 using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Data;
+using System.Data.Linq;
+using System.Web.Script.Serialization;
 
 namespace RegistryToolSvc
 {
@@ -17,10 +20,51 @@ namespace RegistryToolSvc
     // NOTE: If the service is renamed, remember to update the global.asax.cs file
     public class RegistryPatient
     {
-        // using http://www.codeproject.com/Articles/327420/WCF-REST-Service-with-JSON as a template
-        // http://msdn.microsoft.com/en-us/library/ee391967.aspx
+        // resource (old) http://www.codeproject.com/Articles/327420/WCF-REST-Service-with-JSON as a template
+        // resource (old) http://msdn.microsoft.com/en-us/library/ee391967.aspx
+
         [OperationContract]
         [WebInvoke(Method = "GET", UriTemplate = "", RequestFormat = WebMessageFormat.Json,
+                   ResponseFormat = WebMessageFormat.Json)]
+        // TODO: The default Linq way
+        ////http://stackoverflow.com/questions/6834085/returning-datasets-from-linq-to-sql-in-a-rest-wcf-service
+        public List<Patient> GetPatients()
+        {
+            List<Patient> ret = new List<Patient>();
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                var patientResults = from p in context.Patients select p;
+                ret = patientResults.ToList<Patient>();
+            }
+            return ret;
+        }
+
+        // TODO: Using Javascript Serializer to send back custom data
+        //public string ListPatients()
+        //{
+        //    List<Patient> patients = new List<Patient>();
+        //    string json;
+        //    using (DataClasses1DataContext context = new DataClasses1DataContext())
+        //    {
+        //        var patientResults = from p in context.Patients
+        //                             select new { p.First, p.Last, p.DOB };
+        //        CustomTypeResolver resolver = new CustomTypeResolver();
+        //        JavaScriptSerializer jss = new JavaScriptSerializer();
+                
+
+        //        json = jss.Serialize(patientResults,
+        //    }
+        //    return json;
+        //}
+
+        ////http://stackoverflow.com/questions/6834085/returning-datasets-from-linq-to-sql-in-a-rest-wcf-service
+        //String connection = "Data Source=sql2k805.discountasp.net;Initial Catalog=SQL2008R2_838447_dataregistry;User ID=SQL2008R2_838447_dataregistry_user;Password=ssiq778";
+        //System.Data.SqlClient.SqlConnection  sqlconn = new System.Data.SqlClient.SqlConnection(connection);
+        //var MyDataContext = new DataContext(sqlconn);
+        
+        
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/foo", RequestFormat = WebMessageFormat.Json,
                    ResponseFormat = WebMessageFormat.Json)]
         public RegistryPatientObject List()
         {
@@ -72,4 +116,24 @@ namespace RegistryToolSvc
         public string last_name { get; set; }
         public string first_name { get; set; }
     }
+
+    public class CustomTypeResolver : JavaScriptTypeResolver
+    {
+        public override Type ResolveType(string id)
+        {
+            return Type.GetType(id);
+        }
+
+        public override string ResolveTypeId(Type type)
+        {
+            return "Hello Custom writer";
+            //if (type == null)
+            //{
+            //    throw new ArgumentNullException("type");
+            //}
+
+            //return type.Name;
+        }
+    }
+
 }
